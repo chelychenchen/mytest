5db29b304932770f72f74d46fd3ff2424c4a0f98
chelychenchen <853114920@qq.com>
2019-09-05 11:58:08 +0800

    这是我提交到本地仓库的代码   第一次使用git

---
+++ src/com/chenchen/test/GitTest.java
@@ -0,0 +1,16 @@
+package com.chenchen.test;
+
+/**
+ * @author ：CHEN
+ * @date ：Created in 2019/9/5
+ * @description ：
+ * @version: 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("我现在在练习Git");
+        System.out.println("学习学习再学习!");
+        System.out.println("努力努力在努力!");
+    }
+}
